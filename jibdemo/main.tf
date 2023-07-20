d1ff7d9e22008950e5fa0e982d73b35c16c936a3
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-07-20 17:17:29 +0530

    Add files via upload

---
+++ jibdemo/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devopsbyexample-tf-state"
+    key    = "platform.tfstate"
+    region = "us-east-1"
+  }
+}
+
